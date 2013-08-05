3fdf2110720b7cf4abebe8b598a03c7aa3df8219
Andres Gatjens <andresgg@thehangar.cr>
2013-08-05 11:11:26 -0600

    RPS - TEST

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,7 @@
 (function($) {
 $(document).ready(function() {
 	
-	// Nav Manu / Change of images
-  	$('ul.nav li').on('click', function(e) {
-  		e.preventDefault();
-  		$('li').removeClass('active');
-  		$(this).addClass('active');
-  		var choise = $(this).data("option");
-  		$('.jumbotron').css({
-  			'background':'url(' + 'images/' +  choise + '.png )',
-  			'background-position': '50% 50%',
-			'background-repeat': 'no-repeat'
-  		});
-  	});
+  
+
 });
 })(jQuery);
