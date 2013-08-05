3f2bba0be920ae6074a88c3d427507c60b6e6cb8
Andres Gatjens <andresgg@thehangar.cr>
2013-08-05 15:58:05 -0600

    Structure for the Leap Added

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,28 @@
 (function($) {
 $(document).ready(function() {
-	
-  
+
+
+  // Counter
+  $('a.btn-success').on('click', function(e) {
+    $(this).unbind('click');
+    e.preventDefault();
+    $(this).addClass('action');
+
+    var count = 3;
+
+    countdown = setInterval(function(){
+      $('a.btn-success').html(count);
+      if (count == 0) {
+       $('a.btn-success').html("Go!");
+       return;
+      }
+      count--;
+    }, 1000);
+
+  });
+
+  // User name
+  var user_name = $('#user_name').val();
 
 });
 })(jQuery);
